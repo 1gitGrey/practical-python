2380fa4cd1ff49bf0b818270d951cf8cc521952b
Grey Grissom <grey.grissom@gmail.com>
2020-10-24 01:10:53 -0700

    finalized exercise 1 bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+
+
+# declares step variable for loop
+n = 0 # the # of drops
+h = 100 # m | initial height
+
+# loop
+while n < 10:
+    h = h * (3/5)
+    print(n + 1, round(h, 4))
+    n += 1
